0c550a750f04748bd7726435c5a388bf9d72e885
homotech <bliam9145@gmail.com>
2024-07-11 09:42:52 +0100

    Add the Hero Image to the Left

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,9 +1,11 @@
+import HeroImage from "./heroImage";
 import HeroText from "./heroText";
 
 export default function Body() {
   return (
     <div className="container flex flex-wrap">
       <HeroText />
+      <HeroImage />
     </div>
   );
 }
--- src/components/heroImage.tsx
+++ src/components/heroImage.tsx
@@ -1,8 +1,39 @@
+import Image from "next/image";
+import ImageOne from "@/public/image-one.jpg";
+import ImageTwo from "@/public/image-two.jpg";
+
 export default function HeroImage() {
+  const divClassNames = "relative";
+  const vibeImages = [
+    {
+      id: 1,
+      image: ImageOne,
+      text: "#rip stop",
+    },
+    {
+      id: 2,
+      image: ImageTwo,
+      text: "#rip stop",
+    },
+  ];
+
   return (
-    <div className="w-full">
-      <div></div>
-      <div></div>
+    <div className="w-full mt-4 flex flex-wrap">
+      {vibeImages.map((vibeImage) => (
+        <div
+          key={vibeImage.id}
+          className="relative w-full rounded-lg overflow-hidden mb-4"
+        >
+          <div className="overlay bebas-neue-regular text-white p-4 flex items-end absolute h-full w-full">
+            <p className="text-6xl">{vibeImage.text}</p>
+          </div>
+          <Image
+            className=""
+            src={vibeImage.image}
+            alt={vibeImage.text}
+          ></Image>
+        </div>
+      ))}
     </div>
   );
 }
